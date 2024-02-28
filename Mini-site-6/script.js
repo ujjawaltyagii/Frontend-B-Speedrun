ae43f997dd3b349d0e5309e35b20c7d66840a31e
ujjawaltyagii <ujjawal2002tyagi@gmail.com>
2024-02-28 17:57:56 +0530

    fixed

--- Mini-site-6/script.js
+++ Mini-site-6/script.js
@@ -1,19 +1,46 @@
-gsap.to(".imgcontainer",{
-    width: "100%",
-    ease: Expo.easeInOut,
-    duration: 2,
-    stagger: 2
-})
+var tl = gsap.timeline({
+    repeat: -1
+});
 
-gsap.to("#text h1", {
-    ease: Expo.easeInOut,
-    stagger: 2.5,
-    top: 0
-})
+tl.
+    to(".imgcontainer",{
+        width: "100%",
+        ease: Expo.easeInOut,
+        duration: 2,
+        stagger: 2
+    }, 'flag')
+    
+    .to("#text h1", {
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: 0
+    }, 'flag')
+    
+    .to("#text h1", {
+        delay: 2,
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: "-100%"
+    }, 'flag')
 
-gsap.to("#text h1", {
-    delay: 2.5,
-    ease: Expo.easeInOut,
-    stagger: 2.5,
-    top: "-100%"
-})
+
+
+// gsap.to(".imgcontainer",{
+//     width: "100%",
+//     ease: Expo.easeInOut,
+//     duration: 2,
+//     stagger: 2
+// })
+
+// gsap.to("#text h1", {
+//     ease: Expo.easeInOut,
+//     stagger: 2.5,
+//     top: 0
+// })
+
+// gsap.to("#text h1", {
+//     delay: 2.5,
+//     ease: Expo.easeInOut,
+//     stagger: 2.5,
+//     top: "-100%"
+// })
